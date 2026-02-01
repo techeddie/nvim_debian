f247a2a3e2dbe8ccaa6a89b73a895368184efda2
Eddie <eddie@directbox.de>
2026-02-01 18:59:40 +0300

    init

--- lua/plugconfig/nvim-tree.lua
+++ lua/plugconfig/nvim-tree.lua
@@ -1,3 +1,4 @@
+
 -- disable netrw at the very start of your init.lua
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
@@ -5,8 +6,8 @@ vim.g.loaded_netrwPlugin = 1
 -- optionally enable 24-bit colour
 vim.opt.termguicolors = true
 
--- -- empty setup using defaults
--- require("nvim-tree").setup()
+-- empty setup using defaults
+require("nvim-tree").setup()
 
 -- OR setup with some options
 require("nvim-tree").setup({
