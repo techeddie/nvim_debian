dbd8c82005c5d18cd372caeb17b68fe5495cfa16
Eddie <eddie@directbox.de>
2026-03-28 22:36:23 +0100

    terminal

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -48,7 +48,7 @@ local opts = { noremap = true, silent = true }
   keymap("n", "<leader>q", ":<C-w>q<CR>") --quit current buffer
   keymap("n", "<leader>r", "<C-w>r") --switch to previous pane
   keymap("n", "<leader>s", ":FloatermNew<CR>") --open new tab
-  keymap("n", "<leader>t", ":FloatermNew<CR>") --open new tab
+  keymap("n", "<leader>t", ":FloatermNew<CR>") --open new terminal
   keymap("n", "<leader>tl", ":Gitsigns toggle_linehl<CR>") --git toggle light highlight
   keymap("n", "<leader>u", "gUU") --make uppercase
   keymap("n", "<leader>w", ":set wrap<CR>") --wrap
