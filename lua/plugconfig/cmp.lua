30328a9a01604d56d3baf597f80b28c2d3aee055
Eddie <eddie@directbox.de>
2026-04-30 21:57:16 +0200

    disabled tab selection

--- lua/plugconfig/cmp.lua
+++ lua/plugconfig/cmp.lua
@@ -106,18 +106,14 @@ cmp.setup {
       "s",
     }),
     ['<Tab>'] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-      elseif luasnip.expand_or_jumpable() then
+      if luasnip.expand_or_jumpable() then
         luasnip.expand_or_jump()
       else
-        fallback()
+        fallback()  -- just insert tab
       end
     end, { 'i', 's' }),
     ['<S-Tab>'] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item()
-      elseif luasnip.jumpable(-1) then
+      if luasnip.jumpable(-1) then
         luasnip.jump(-1)
       else
         fallback()
